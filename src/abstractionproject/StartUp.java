33410cd4f6ff21a80135bcae54d9135ca70dd35c
Adam <Adam@acrist>
2015-09-13 22:41:28 -0500

    fixed some dependances

---
+++ src/abstractionproject/StartUp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractionproject;
+
+/**
+ *
+ * @author Adam
+ */
+public class StartUp {
+    public static void main(String[] args) {
+        
+        Card King = new CardConstructor("King","Red","Hearts",10);
+
+        
+        
+    }
+}
