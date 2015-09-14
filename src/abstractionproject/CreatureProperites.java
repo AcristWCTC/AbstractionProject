5cc4fca2da93f8c2f9706f18016e34061a2ed11f
Adam <Adam@acrist>
2015-09-13 20:56:44 -0500

    Modified Creature again

--- src/abstractionproject/CreatureProperites.java
+++ src/abstractionproject/CreatureProperites.java
@@ -21,63 +21,8 @@ public abstract class CreatureProperites implements Card {
     private String set;
     private int cmc;
     
-    public int getPower() {
-        return power;
-    }
-
-    public void setPower(int power) {
-        this.power = power;
-    }
-
-    public int getToughness() {
-        return toughness;
-    }
-
-    public void setToughness(int toughness) {
-        this.toughness = toughness;
-    }
-    
-       public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setSet(String set) {
-        this.set = set;
-    }
-
-    public void setEffect(String effect) {
-        this.effect = effect;
-    }
-
-    public void setCovertedManaCost(int cmc) {
-        this.cmc = cmc;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getSet() {
-        return set;
-    }
-
-    public String getEffect() {
-        return effect;
-    }
-
-    public int getCovertedManaCost() {
-        return cmc;
-    }
-
-    
-
+    public abstract void power (int power);
+    public abstract void toughness (int toughness);
     
 }
+    
